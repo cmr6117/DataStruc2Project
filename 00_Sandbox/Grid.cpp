559b9955e205cd502bdd7639f3233b2666e46e67
Nick Visconti <nickvisc956@gmail.com>
2019-04-23 12:06:58 -0400

    created grid's init() for creating the grid

--- 00_Sandbox/Grid.cpp
+++ 00_Sandbox/Grid.cpp
@@ -3,11 +3,33 @@ using namespace Simplex;
 
 
 
-Grid::Grid()
+Grid::Grid(float worldRadius)
 {
+	Init(worldRadius);
 }
 
 
 Grid::~Grid()
 {
+}
+
+void Simplex::Grid::Init(float worldRadius)
+{
+	WorldRadius = worldRadius;
+	CellSize = (worldRadius * 2) / NUM_CELLS;
+
+	//create the grid
+	vector3 startPos = vector3(-WorldRadius, 0.0f, -WorldRadius);
+	for (uint i = 0; i < NUM_CELLS; i++)
+	{
+		vector3 rowPos = vector3(startPos.x+(CellSize*i), 0.0f, startPos.z);
+		for (uint j = 0; j < NUM_CELLS; j++)
+		{
+			vector3 cellMin = vector3(rowPos.x, 0.0f, rowPos.z + (CellSize*j));
+			vector3 cellMax = vector3(cellMin.x + CellSize, 0.0f, cellMin.z + CellSize);
+			//create the cell sudocode (until cell node is actually created
+			//CellNode *buffer = new  CellNode(cellMin, cellMax);
+			//CellList.push_back(buffer);
+		}
+	}
 }
--- 00_Sandbox/Grid.h
+++ 00_Sandbox/Grid.h
@@ -1,11 +1,24 @@
 #pragma once
 
+#include "AppClass.h"
+#include "CellNode.h"
+
 namespace Simplex
 {
 	class Grid
 	{
 	public:
-		Grid();
+		Grid(float worldRadius);
 		~Grid();
+
+		//grid functions
+		void Init(float worldRadius);
+
+		//grid variables
+		bool Active;
+		float CellSize;
+		float WorldRadius;
+		static const int NUM_CELLS = 12;
+		std::vector<CellNode> CellList;
 	};
 }
